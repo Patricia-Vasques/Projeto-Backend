56d7e639d78f6f3d55cb2d2ba0d371686dbaf703
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 19:51:11 -0300

    feat: Criei a rota para alterar o status de um depósito

--- src/routes/storehouse.routes.js
+++ src/routes/storehouse.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require ('express')
 const { auth } = require ('../middleware/auth')
-const { createOneStoreHouse, updateStoreHouse} = require('../controllers/storehouses.controllers')
+const { createOneStoreHouse, updateStoreHouse, upstatusOneStoreHouse} = require('../controllers/storehouses.controllers')
 
 class StoreHouseRouter {
     routesFromStoreHouse () {
         const storeHouseRoutes = Router()
-            storeHouseRoutes.post('/depositos', auth, createOneStoreHouse)
-            storeHouseRoutes.patch('/depositos/:id', auth, updateStoreHouse)
+            storeHouseRoutes.post('/depositos', auth, createOneStoreHouse),
+            storeHouseRoutes.patch('/depositos/:id', auth, updateStoreHouse),
+            storeHouseRoutes.patch('/depositos/:id/status', auth, upstatusOneStoreHouse)
 
             return storeHouseRoutes
     }
