1131ed87d37f029b921491dc0e9bd43df9fde74c
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 20:13:47 -0300

    feat: Criei a rota para deletar um depósito

--- src/routes/storehouse.routes.js
+++ src/routes/storehouse.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require ('express')
 const { auth } = require ('../middleware/auth')
-const { createOneStoreHouse, updateStoreHouse, upstatusOneStoreHouse, listStoreHouseStatus, listStoreHouseId} = require('../controllers/storehouses.controllers')
+const { createOneStoreHouse, updateStoreHouse, upstatusOneStoreHouse, listStoreHouseStatus, listStoreHouseId, deleteStoreHouse} = require('../controllers/storehouses.controllers')
 
 class StoreHouseRouter {
     routesFromStoreHouse () {
@@ -8,8 +8,10 @@ class StoreHouseRouter {
             storeHouseRoutes.post('/depositos', auth, createOneStoreHouse),
             storeHouseRoutes.patch('/depositos/:id', auth, updateStoreHouse),
             storeHouseRoutes.patch('/depositos/:id/status', auth, upstatusOneStoreHouse),
-            storeHouseRoutes.get('/depositos', auth, listStoreHouseStatus)
-            storeHouseRoutes.get('/depositos/:id', auth, listStoreHouseId)
+            storeHouseRoutes.get('/depositos', auth, listStoreHouseStatus),
+            storeHouseRoutes.get('/depositos/:id', auth, listStoreHouseId),
+            storeHouseRoutes.delete('/depositos/:id', auth, deleteStoreHouse)
+            
             return storeHouseRoutes
     }
 }
