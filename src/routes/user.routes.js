35b03837e1d3245919769bfa9890fb3156389179
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 15:28:26 -0300

    feat: Criei a rota do login do usuário

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
-const { createOneUser } = require ('../controllers/user.controllers')
+const { createOneUser, loginUser } = require ('../controllers/user.controllers')
 
 class UserRouter {
     routesFromUser () {
         const userRoutes = Router()
-        userRoutes.post('/usuarios', createOneUser)
+        userRoutes.post('/usuarios', createOneUser),
+        userRoutes.post('/usuarios/login',loginUser)
 
         return userRoutes
     }
