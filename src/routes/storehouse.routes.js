2d6dd68ce5dd47a856df54ab1edec2641d84e04c
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 19:59:57 -0300

    feat: Criei a rota para o endpoint de listar depósitos pelo status

--- src/routes/storehouse.routes.js
+++ src/routes/storehouse.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require ('express')
 const { auth } = require ('../middleware/auth')
-const { createOneStoreHouse, updateStoreHouse, upstatusOneStoreHouse} = require('../controllers/storehouses.controllers')
+const { createOneStoreHouse, updateStoreHouse, upstatusOneStoreHouse, listStoreHouseStatus} = require('../controllers/storehouses.controllers')
 
 class StoreHouseRouter {
     routesFromStoreHouse () {
         const storeHouseRoutes = Router()
             storeHouseRoutes.post('/depositos', auth, createOneStoreHouse),
             storeHouseRoutes.patch('/depositos/:id', auth, updateStoreHouse),
-            storeHouseRoutes.patch('/depositos/:id/status', auth, upstatusOneStoreHouse)
+            storeHouseRoutes.patch('/depositos/:id/status', auth, upstatusOneStoreHouse),
+            storeHouseRoutes.get('/depositos', auth, listStoreHouseStatus)
 
             return storeHouseRoutes
     }
