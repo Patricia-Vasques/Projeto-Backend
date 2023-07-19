56636fa93d5ce6f8c86dd9ab2f4349c30ee57087
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 19:42:16 -0300

    feat: Criei a rota do endpoint e para alterar dados de um depósito pelo id

--- src/routes/storehouse.routes.js
+++ src/routes/storehouse.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require ('express')
 const { auth } = require ('../middleware/auth')
-const { createOneStoreHouse } = require('../controllers/storehouses.controllers')
+const { createOneStoreHouse, updateStoreHouse} = require('../controllers/storehouses.controllers')
 
 class StoreHouseRouter {
     routesFromStoreHouse () {
         const storeHouseRoutes = Router()
             storeHouseRoutes.post('/depositos', auth, createOneStoreHouse)
+            storeHouseRoutes.patch('/depositos/:id', auth, updateStoreHouse)
 
             return storeHouseRoutes
     }
