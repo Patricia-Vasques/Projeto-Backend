0255479b775fcb2df01068233f8aeddc0c97d1e3
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 21:21:05 -0300

    feat: Criei a rota do endpoint de listar medicamentos pelo tipo

--- src/routes/medication.routes.js
+++ src/routes/medication.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require ('express')
 const { auth }= require ('../middleware/auth')
-const { CreateOneMedication, updateMedication } = require('../controllers/medication.controllers')
+const { CreateOneMedication, updateMedication, listMedicationType } = require('../controllers/medication.controllers')
 
 class MedicationRouter {
     routesFromMedication() {
         const medicationRoutes = Router()
         medicationRoutes.post('/medicamentos', auth, CreateOneMedication),
-        medicationRoutes.put('/medicamentos/:id', auth, updateMedication)
+        medicationRoutes.put('/medicamentos/:id', auth, updateMedication),
+        medicationRoutes.get('/medicamentos', auth, listMedicationType)
 
         return medicationRoutes
     }
