c350358c85cdecc4f09fe7bf90233066f43a6bda
Patrícia Vasques <patriciahhhh@gmail.com>
2023-07-19 21:27:32 -0300

    feat: Criei o endpoint para deletar um medicamento

--- src/routes/medication.routes.js
+++ src/routes/medication.routes.js
@@ -1,13 +1,15 @@
 const { Router } = require ('express')
 const { auth }= require ('../middleware/auth')
-const { CreateOneMedication, updateMedication, listMedicationType } = require('../controllers/medication.controllers')
+const { CreateOneMedication, updateMedication, listMedicationType, listMedicationId, deleteOneMedication } = require('../controllers/medication.controllers')
 
 class MedicationRouter {
     routesFromMedication() {
         const medicationRoutes = Router()
         medicationRoutes.post('/medicamentos', auth, CreateOneMedication),
         medicationRoutes.put('/medicamentos/:id', auth, updateMedication),
-        medicationRoutes.get('/medicamentos', auth, listMedicationType)
+        medicationRoutes.get('/medicamentos', auth, listMedicationType),
+        medicationRoutes.get('/medicamentos/:id', auth, listMedicationId),
+        medicationRoutes.delete('/medicamentos/:id', auth, deleteOneMedication)
 
         return medicationRoutes
     }
